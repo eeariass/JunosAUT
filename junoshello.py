7e290470b554fbf6d132ffc51c3f500f96f601b2
Arias <elvarias@a483e78b990c.ant.amazon.com>
2020-06-12 00:27:14 +0100

    testing junos Device class from PyEZ

---
+++ junoshello.py
@@ -0,0 +1,11 @@
+#! /usr/local/bin/python3 
+import pprint
+from jnpr.junos import Device
+import json
+
+dev = Device(host='192.168.0.10', user='root', password='lab123')
+dev.open()
+junosGeneralInfo = dev.facts()
+dev.close()
+
+pprint(junosGeneralInfo)
