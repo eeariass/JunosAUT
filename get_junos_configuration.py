c3d2b27a1eb8b7525d853991692d6816f75c0b89
Arias <elvarias@a483e78b990c.ant.amazon.com>
2020-06-13 17:45:48 +0100

    Get XML and convert it into JSON

--- get_junos_configuration.py
+++ get_junos_configuration.py
@@ -1,10 +1,12 @@
 #! /usr/local/bin/python3
 
+import xmltodict
 import sys
 from pprint import pprint
 from getpass import getpass
-from jnpr.junos import Device
+from jnpr.junos import *
 from lxml import etree
+import json
 
 hostname = input('insert hostname or ip address: ')
 username = input ('insert username: ')
@@ -15,5 +17,6 @@ print('')
 print('*** Device general information: ***')
 
 with Device(host=hostname, user=username, password=passwd) as dev:
-    config = dev.rpc.get_config(filter_xml=etree.XML('<configuration></configuration>'))
-    print(etree.tostring(config))
+    configuration = dev.rpc.get_config(filter_xml=etree.XML('<configuration><interfaces/></configuration>'))
+    toJson = json.dumps(xmltodict.parse((etree.tostring(configuration))))
+    pprint(toJson)
